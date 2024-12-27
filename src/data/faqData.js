181c0ed49e4ce5530787c3c17e95fc4e38ab2aaf
nazrindaniell <nazrindaniel8@gmail.com>
2024-12-28 04:45:23 +0800

    creating faq data to store and fetch contents

---
+++ src/data/faqData.js
@@ -0,0 +1,24 @@
+const faqData = [
+  {
+    heading: "Lorem ipsum dolor sit amet, consectetur",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris.",
+  },
+  {
+    heading: "Sed do eiusmod tempor incididunt ut labore",
+    answer:
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    heading: "Duis aute irure dolor in reprehenderit",
+    answer:
+      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    heading: "Ut enim ad minim veniam, quis nostrud",
+    answer:
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+];
+
+export default faqData;
