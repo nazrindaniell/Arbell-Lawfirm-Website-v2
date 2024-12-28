2d1bb7b8af6b6d88bf428553d13c13bb9f0c7fdc
nazrindaniell <nazrindaniel8@gmail.com>
2024-12-29 00:56:10 +0800

    added Header and GalleryHeader components to the page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,20 @@
+import Header from "../components/Header";
+import GalleryHeader from "../components/GalleryHeader";
+import GalleryImg from "../assets/sample-img.webp";
+
 function Contact() {
   return (
     <>
-      <h1>Contact</h1>
-      <p>This is the contact page</p>
+      <section className="container mx-auto py-12 lg:py-20">
+        <Header
+          heading="Contact Us."
+          subheading="Get in touch with us for expert guidance and personalized support."
+          desc="At Arbell, we provide a complete suite of legal services designed to resolve disputes and protect your interests. Whether navigating complex litigation, reaching strategic agreements through negotiation, or finding common ground in mediation, our experts are here to support you. With a commitment to excellece and a focus on tailored solutions, we help you move forward confidently, no matter the challenge."
+        />
+      </section>
+      <section>
+        <GalleryHeader imgSrc={GalleryImg} />
+      </section>
     </>
   );
 }
