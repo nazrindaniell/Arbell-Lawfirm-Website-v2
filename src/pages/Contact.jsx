702d115a4e6ec716b72eb33cbe2404f5d5a70402
nazrindaniell <nazrindaniel8@gmail.com>
2024-12-30 02:08:11 +0800

    created interface for contact page using prebuild components

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,13 @@
 import Header from "../components/Header";
 import GalleryHeader from "../components/GalleryHeader";
 import GalleryImg from "../assets/sample-img.webp";
+import ContactDetails from "../components/ContactDetails";
+import ContactSocialMedia from "../components/ContactSocialMedia";
 
 function Contact() {
   return (
     <>
+      {/* header */}
       <section className="container mx-auto py-12 lg:py-20">
         <Header
           heading="Contact Us."
@@ -12,8 +15,16 @@ function Contact() {
           desc="At Arbell, we provide a complete suite of legal services designed to resolve disputes and protect your interests. Whether navigating complex litigation, reaching strategic agreements through negotiation, or finding common ground in mediation, our experts are here to support you. With a commitment to excellece and a focus on tailored solutions, we help you move forward confidently, no matter the challenge."
         />
       </section>
+      {/* gallery header */}
       <section>
         <GalleryHeader imgSrc={GalleryImg} />
+      </section>
+      {/* contact details */}
+      <section className="container mx-auto mt-16 py-10 lg:mb-8 lg:mt-24 lg:py-20">
+        <div className="flex flex-col gap-14 md:gap-5">
+          <ContactDetails />
+          <ContactSocialMedia />
+        </div>
       </section>
     </>
   );
