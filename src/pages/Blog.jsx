7887428a788c8bd43c60019eb3541ff1030fc321
nazrindaniell <nazrindaniel8@gmail.com>
2025-01-01 21:18:08 +0800

    added contentful component to blog page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,8 +3,9 @@ import GalleryHeader from "../components/GalleryHeader";
 import GalleryImg from "../assets/sample-img.webp";
 import Partnership from "../components/Partnership";
 import Faq from "../components/Faq";
-import LearnMoreButton from "../components/LearnMoreButton";
-import PodcastImg from "../assets/podcast-sample.webp";
+
+import Posts from "./Posts";
+import LatestBlogPost from "./LatestSingleBlog";
 
 function Blog() {
   return (
@@ -26,34 +27,13 @@ function Blog() {
       <section className="container mx-auto mt-16 py-10 lg:mb-8 lg:mt-24 lg:py-20">
         <div className="flex flex-col gap-10">
           <h1 className="font-serif text-4xl lg:text-5xl">Latest Updates.</h1>
-          <div className="grid grid-cols-1 place-items-center gap-8 lg:grid-cols-2 lg:gap-10">
-            <div className="h-full w-full">
-              <img
-                src={PodcastImg}
-                alt="Blog Img"
-                className="h-full w-full object-cover object-center"
-              />
-            </div>
-            <div className="flex flex-col gap-6">
-              <h2 className="font-serif text-3xl lg:text-4xl">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et
-                massa mi.
-              </h2>
-              <p className="text-base text-black-200 lg:text-lg">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et
-                massa mi. Aliquam in hendrerit urna. Pellentesque sit amet
-                sapien fringilla, mattis ligula consectetur, ultrices mauris.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et
-                massa mi. Aliquam in hendrerit urna. Pellentesque sit amet
-                sapien fringilla, mattis ligula consectetur, ultrices
-                mauris.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet
-                sapien fringilla, mattis ligula consectetur, ultrices mauris.
-              </p>
-              <LearnMoreButton to="#">Learn More</LearnMoreButton>
-            </div>
-          </div>
+          <LatestBlogPost />
         </div>
+      </section>
+
+      {/* Blog section */}
+      <section className="container mx-auto my-11">
+        <Posts />
       </section>
 
       {/* who trusted us */}
