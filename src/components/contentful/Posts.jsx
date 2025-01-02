b2c0ae400a391b589b19fee9432d7e2b7aaf5b13
nazrindaniell <nazrindaniel8@gmail.com>
2025-01-03 03:23:08 +0800

    added swiper carousel to contentful blog posts

--- src/components/contentful/Posts.jsx
+++ src/components/contentful/Posts.jsx
@@ -1,6 +1,14 @@
 import { useEffect, useState } from "react";
 import LearnMoreButton from "../LearnMoreButton";
 import { getBlogPosts } from "/contentful";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { RiArrowRightSFill, RiArrowLeftSFill } from "react-icons/ri";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 function Posts() {
   const [posts, setPosts] = useState([]);
@@ -27,30 +35,78 @@ function Posts() {
     if (error) return <p>Error: {error}</p>;
 
     return posts.map((post) => (
-      <div className="flex flex-1" key={post.sys.id}>
-        <div className="flex flex-1 flex-col justify-between gap-5">
-          <img
-            className="w-full object-cover object-center lg:h-72"
-            src={post.fields.image.fields.file.url}
-            alt={post.fields.title}
-          />
-          <h3 className="line-clamp-1 font-serif text-2xl lg:text-3xl">
-            {post.fields.title}
-          </h3>
-          <p className="line-clamp-5 text-base text-black-200 lg:text-lg">
-            {post.fields.paragraph}
-          </p>
-          <LearnMoreButton to={post.fields.slug}>Learn More</LearnMoreButton>
+      <SwiperSlide key={post.sys.id}>
+        <div className="flex flex-1">
+          <div className="flex flex-1 flex-col justify-between gap-5">
+            <img
+              className="w-full object-cover object-center lg:h-72"
+              src={post.fields.image.fields.file.url}
+              alt={post.fields.title}
+            />
+            <h3 className="line-clamp-1 font-serif text-2xl lg:text-3xl">
+              {post.fields.title}
+            </h3>
+            <p className="line-clamp-5 text-base text-black-200 lg:text-lg">
+              {post.fields.paragraph}
+            </p>
+            <LearnMoreButton to={`/blog/${post.fields.slug}`}>
+              Learn More
+            </LearnMoreButton>
+          </div>
         </div>
-      </div>
+      </SwiperSlide>
     ));
   };
 
   return (
     <div className="flex flex-col gap-8">
       <h1 className="font-serif text-4xl lg:text-5xl">More Updates.</h1>
-      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-        {renderPosts()}
+      <div className="relative w-full">
+        <Swiper
+          // install Swiper modules
+          modules={[Navigation, Pagination, A11y]}
+          spaceBetween={50}
+          slidesPerView={2}
+          pagination={{
+            clickable: true,
+            el: ".swiper-pagination",
+          }}
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+              pagination: {
+                clickable: true,
+              },
+              navigation: false,
+              autoHeight: true,
+            },
+            1024: {
+              slidesPerView: 2,
+              pagination: false,
+              navigation: {
+                nextEl: ".custom-swiper-button-next",
+                prevEl: ".custom-swiper-button-prev",
+              },
+            },
+            1280: {
+              slidesPerView: 3,
+              pagination: false,
+              navigation: {
+                nextEl: ".custom-swiper-button-next",
+                prevEl: ".custom-swiper-button-prev",
+              },
+            },
+          }}
+        >
+          {renderPosts()}
+        </Swiper>
+        <div className="custom-swiper-button-prev hidden lg:block">
+          <RiArrowLeftSFill size={80} />
+        </div>
+        <div className="custom-swiper-button-next hidden lg:block">
+          <RiArrowRightSFill size={80} />
+        </div>
+        <div className="swiper-pagination !-bottom-12 block lg:hidden"></div>
       </div>
     </div>
   );
