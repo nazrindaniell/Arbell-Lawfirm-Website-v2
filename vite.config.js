8e1f4a7b79cf69fe72368183f6fcf93b129eba81
nazrindaniell <nazrindaniel8@gmail.com>
2025-01-06 17:34:34 +0800

    fix navigation error

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   define: {
-//     "process.env": process.env,
-//   },
-// });
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
@@ -16,5 +8,8 @@ export default defineConfig(({ mode }) => {
       "process.env": env,
     },
     plugins: [react()],
+    server: {
+      historyApiFallback: true,
+    },
   };
 });
